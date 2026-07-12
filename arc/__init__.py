f1d4bc2527455c4b507c30b25ca17908c901ad27
Arc Test <test@arc.dev>
2026-07-12 20:36:54 +0530

    init

--- arc/__init__.py
+++
@@ -1,6 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("arc-prs")
-except PackageNotFoundError:  # source checkout without `pip install -e .`
-    __version__ = "0.0.0+dev"
